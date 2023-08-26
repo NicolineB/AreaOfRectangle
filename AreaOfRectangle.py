aafe580d025b86038e45f49b94ce7c49dfdd3942
Nico <broloes2@gmail.com>
2023-08-26 14:04:27 +0200

    Added script: Retangle area calcualtion

---
+++ AreaOfRectangle.py
@@ -0,0 +1,12 @@
+# Create a simple Python program that calculates and displays the area of a rectangle.
+
+
+# Defining input from terminal, and formatting the string we get to int
+length = int(input("Enter number: "))
+width = int(input("Enter width: "))
+
+# Calculates the length and width of rectangle
+area = length * width
+
+#print out the area calculation
+print("The area of a rectangle with given numbers are: ", area)
